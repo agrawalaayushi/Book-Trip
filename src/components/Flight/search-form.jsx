346f0cd188b27723ab7fcc0ae73a64fc3a607b52
agrawal.aayushi <agrawal.aayushi13@.com>
2018-09-30 09:03:57 +0530

    Search form UI created

--- src/components/Flight/search-form.jsx
+++ src/components/Flight/search-form.jsx
@@ -34,7 +34,36 @@ class SearchForm extends Component {
   render() {
     return (
       <div className="flight-search-form">
-        Search form
+        <div className="search-box">
+          <form>
+            <div className="button-wrapper">
+              <input type="button" value="One Way" />
+              <input type="button" value="Return" />
+            </div>
+            <div className="search-form-wrapper">
+              <input type="text" name="firstname" placeholder="Enter origin city" />
+              <input type="text" name="firstname" placeholder="Enter Destination city" />
+              <input type="date" name="firstname" placeholder="Departure date" />
+              <input type="date" name="firstname" placeholder="Return date" />
+              <select>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+              </select> 
+              <input type="submit" value="Search" />
+            </div>
+          </form>
+        </div>
+        <div className="refine-price-wrapper">
+          Refine flight search
+
+        </div>
       </div>
     );
   }
