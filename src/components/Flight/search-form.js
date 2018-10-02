b052714fccfcc481836fcf5b0c9ba7b01604495c
agrawal.aayushi <agrawal.aayushi13@.com>
2018-10-02 14:51:22 +0530

    Handled onchange search details

--- src/components/Flight/search-form.js
+++ src/components/Flight/search-form.js
@@ -2,7 +2,6 @@ import React, { Component }  from 'react'
 import { connect } from 'react-redux';
 import { isEqual } from 'lodash';
 import { Header } from '../Header/header';
-import SearchBar from '../common/search-bar';
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import { formatDate, parseDate } from 'react-day-picker/moment';
 import "react-day-picker/lib/style.css";
@@ -16,25 +15,31 @@ class SearchForm extends Component {
       fields:{
         origin: '',
         destination: '',
-        end_date: '',
-        start_date: ''
+        passenger: 0,
+        return_date: '',
+        dep_date: '',
+        tripType: 'oneWay'
       },
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTripTypeChange = this.handleTripTypeChange.bind(this);
   }
 
   //-----------------------------------
   // Methods
   //-----------------------------------
 
-  handleSearchMovies(query) {
+  handleChange(field, e){
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
     this.setState({
-      searchKeyword: query
-    })
-    // this.props.handleSearchMovies(query);
+      fields,
+      errors: {}
+    });
   }
 
   handleStartDate(field, date) {
-    const {isError} = this.state;
     let fields = this.state.fields;
     fields[field] = date;
     this.setState({
@@ -44,14 +49,74 @@ class SearchForm extends Component {
   };
 
   handleReturnDate(field, date) {
-    const {isError} = this.state;
-    let fields = this.state.fields;
+    let {fields} = this.state;
     fields[field] = date;
     this.setState({
       fields,
       errors: {}
     });
   };
+  
+  handleTripTypeChange(e) {
+    let fields = this.state;
+    fields['tripType'] = e.target.value;
+    this.setState({ 
+      fields, 
+    })
+  }
+
+  // handleUploadChoice = (e, { value }) => this.setState({ value, selectUploadChoice: value });
+
+
+  handleFormValidation(fields) {
+    let errors = {};
+    let formIsValid = true;
+    let result = {};
+
+    if(fields["origin"] === '') {
+      formIsValid = false;
+      errors["origin"] = "Please provide origin";
+    } else if(fields["destination"] === '') {
+      formIsValid = false;
+      errors["destination"] = "Please provide destination";
+    } else if(fields["dep_date"] === '') {
+      formIsValid = false;
+      errors["dep_date"] = "Please provide date of journey";
+    } else if(fields["passenger"] === ''){
+      formIsValid = false;
+      errors["passenger"] = "Please provide passenger";
+    } 
+    result['errors'] = errors;
+    result['isFormValid'] = formIsValid;
+    return result;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { fields, isError } = this.state;
+    const formValidationFeedback = this.handleFormValidation(fields);
+
+    if(formValidationFeedback.isFormValid) {
+      const params={
+        tripType: fields.tripType,
+        origin: fields.origin,
+        destination: fields.destination,
+        passenger: fields.passenger
+      };
+      if(fields.dep_date) {
+        params['dep_date'] = getFormatedDate(fields.dep_date);
+      }
+      if(fields.tripType === 'return' && fields.return_date) {
+        params['return_date'] = getFormatedDate(fields.return_date);
+      }
+      // this.setState({
+      //   isLoading: true
+      // })
+      this.props.searchDetailsCallback(params);
+    } else{
+      this.setState({errors:formValidationFeedback.errors, isLoading: false});
+    }
+  }
   
   //-----------------------------------
   // Lifecycle
@@ -62,19 +127,32 @@ class SearchForm extends Component {
   }
 
   render() {
+    const { tripType } = this.state.fields;
     return (
       <div className="flight-search-form">
         <div className="search-box">
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div className="button-wrapper">
-              <input type="button" value="One Way" />
-              <input type="button" value="Return" />
+              <input 
+              type="radio" 
+              name="oneWay" 
+              value="oneWay"
+              defaultChecked
+              checked={this.state.fields["tripType"] === "oneWay"}
+              onChange={this.handleTripTypeChange} /> One Way
+
+              <input
+              type="radio" 
+              name="return" 
+              value="return"
+              checked={this.state.fields["tripType"] === "return"} 
+              onChange={this.handleTripTypeChange}  /> Return  
             </div>
             <div className="search-form-wrapper">
-              <input type="text" name="firstname" placeholder="Enter origin city" />
-              <input type="text" name="firstname" placeholder="Enter Destination city" />
-              {/* <input type="date" name="firstname" placeholder="Enter Destination city" />
-              <input type="date" name="firstname" placeholder="Enter Destination city" /> */}
+              <input type="text" name="origin" placeholder="Enter Origin city" value={this.state.fields["origin"]}  
+              onChange={this.handleChange.bind(this, 'origin')} />
+              <input type="text" name="destination" placeholder="Enter Destination city" value={this.state.fields["destination"]}  
+              onChange={this.handleChange.bind(this, 'destination')} />
 
               <div className='date-picker-wrapper'>
                 <DayPickerInput
@@ -82,8 +160,8 @@ class SearchForm extends Component {
                   formatDate={formatDate}
                   parseDate={parseDate}
                   placeholder="Start Date"
-                  onDayChange={this.handleStartDate.bind(this, 'start_date')}
-                  value={getFormatedDate(this.state.fields["start_date"])}
+                  onDayChange={this.handleStartDate.bind(this, 'dep_date')}
+                  value={getFormatedDate(this.state.fields["dep_date"])}
                   inputProps={{readOnly: true}}
                 />
                 <DayPickerInput
@@ -96,7 +174,7 @@ class SearchForm extends Component {
                   inputProps={{readOnly: true}}
                 />
               </div>
-              <select>
+              <select placeholder="Passenger" value="Passenger"> 
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -107,7 +185,7 @@ class SearchForm extends Component {
                 <option value="8">8</option>
                 <option value="9">9</option>
               </select> 
-              <input type="submit" value="Search" />
+              <input type="submit" value="Search" onClick={this.handleSubmit}/>
             </div>
           </form>
         </div>
